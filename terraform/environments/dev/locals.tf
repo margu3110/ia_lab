691d1b778e4955e4afb5e86241a0b1a9536d36ad
Marcelo Arguello <marcelo.arguello@gmail.com>
2026-07-26 04:01:29 -0300

    sprint 1

---
+++ terraform/environments/dev/locals.tf
@@ -0,0 +1,12 @@
+locals {
+  project_name = "ia-lab"
+  environment  = "dev"
+
+  common_tags = {
+    Project     = local.project_name
+    Environment = local.environment
+    ManagedBy   = "Terraform"
+    Owner       = "Marcelo"
+    Repository  = "ia_lab"
+  }
+}
